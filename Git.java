740a702c85e945d81a452704c2fc77c8c7db2b8c
Rajvardhan <rajvardhanp11103@gmail.com>
2024-12-07 21:21:18 +0530

    Ading in NewBranch1

--- Git.java
+++ Git.java
@@ -1,10 +1,7 @@
 public class Git {
     public static void main(String [] args){
-        System.out.println("final in MASTER branch");
-        System.out.println(" This code in new breamch");
-        System.out.println(" i am using git  1st time.....!!!");
-        System.out.println("noe adding into git");
-        System.out.println("....");
+        System.out.println("  !!!!  In NewBranch1 !!!   ");
         System.out.println(" @@@@@@@@@@@@@ ");
+
     }
 }
